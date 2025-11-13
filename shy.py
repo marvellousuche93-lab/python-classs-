279acc688d882c944debf38d086f52dd9a16c163
Marvellous <marvellousuche93@gmail.com>
2025-11-12 23:33:56 -0800

    add some new change

--- shy.py
+++ shy.py
@@ -1,1 +1,37 @@
-hjkrfffff
+# append()
+
+name = ["Paul", "Victor", "David"]
+name .append('Marvellous')
+print(name)
+
+# Clear()
+
+name = ["Paul", "Victor", "David"]
+name.clear()
+print(name)
+
+# Copy()
+name = ["Paul", "Victor", "David"]
+name.copy()
+print(name)
+
+# count()
+name = "Grace is a good girl and Grace is a good friend"
+print(name.count('Grace'))
+
+# extend()
+name = ["Paul", "Victor", "David"]
+
+cars = ["Camry", "Tayota", "Benz"]
+name.extend(cars)
+print(name)
+
+# index()
+name = ["Marvellous is fun"]
+print(name[0].index ("fun"))
+
+# insert()
+names = ["Paul", "Victor", "David"]
+names.insert(0, "Marvellous")
+print(names)
+
